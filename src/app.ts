9f3bb9b756ab962807c469f58f9029c95d655240
gutsgon <gustavogoncalves.contato1@gmail.com>
2025-08-17 20:55:19 -0300

    feat: Netlify serverless add

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-app.use(routes)
+app.use('/api', routes)
 
 export { app };
