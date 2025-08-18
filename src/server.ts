c5c77f85632c05184e7939f4d1344b8337edc8e2
gutsgon <gustavogoncalves.contato1@gmail.com>
2025-08-18 00:02:31 -0300

    feat: render deploy

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { app } from "./app";
 
 // Configuração do servidor
-const host = process.env.HOST;
-const port = process.env.PORT;
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://${host}:${port}`);
