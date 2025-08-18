20f5e89fdeda9051e0f210c55ca946b0a5ad4b67
gutsgon <gustavogoncalves.contato1@gmail.com>
2025-08-17 23:07:14 -0300

    feat: render deploy

--- backend/dist/data/db.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-const index_1 = require("./../../../src/data/generated/prisma/index");
-exports.db = new index_1.PrismaClient();
--- backend/dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-// Configuração do servidor
-const host = process.env.HOST;
-const port = process.env.PORT;
-app_1.app.listen(port, () => {
-    console.log(`Servidor rodando em http://${host}:${port}`);
-});
