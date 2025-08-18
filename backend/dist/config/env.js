01c5933ad380dbdd03ff52a0fbb90458ebf6eb08
gutsgon <gustavogoncalves.contato1@gmail.com>
2025-08-17 22:48:33 -0300

    feat: render deploy

--- backend/dist/config/env.js
+++ backend/dist/config/env.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = require("path");
 const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)({ path: (0, path_1.resolve)(__dirname, './../../.env') });
+(0, dotenv_1.config)({ path: (0, path_1.resolve)(__dirname, './../../../.env') });
--- backend/dist/data/db.js
+++ backend/dist/data/db.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
-const index_1 = require("./generated/prisma/index");
+const index_1 = require("./../../../src/data/generated/prisma/index");
 exports.db = new index_1.PrismaClient();
